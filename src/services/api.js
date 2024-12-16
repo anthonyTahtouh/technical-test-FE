85ffa4d0b65b93bc0d347c8630f518cafda1dd77
Anthony tahtouh <anthony1.tahtouh@gmail.com>
2024-12-16 17:35:40 +0200

    #production base url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: 'https://46.101.252.244/api',
 });
 
 API.interceptors.request.use((req) => {
