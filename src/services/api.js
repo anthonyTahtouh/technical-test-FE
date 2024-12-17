0ad0ad1bce54dc83f4cc83968dbcd353363d879f
Anthony tahtouh <anthony1.tahtouh@gmail.com>
2024-12-17 20:40:21 +0200

    fix the baseurl

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://46.101.252.244/api',
+  baseURL: 'http://46.101.252.244/api/',
 });
 
 API.interceptors.request.use((req) => {
